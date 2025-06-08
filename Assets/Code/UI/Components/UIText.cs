5b60ee75adf43d10db17d5aea8600e08186012ba
=) <xxx.awdsasdaw.xxx@gmail.com>
2025-06-08 23:12:15 +0300

    main menu view wip

--- Assets/Code/UI/Components/UIText.cs
+++ Assets/Code/UI/Components/UIText.cs
@@ -1,15 +1,25 @@
-﻿using TMPro;
+﻿using System;
+using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 namespace UI.Components
 {
-    public class UIText : MonoBehaviour
+    public class UIText : MonoBehaviour, IPointerDownHandler
     {
+        public event Action Clicked;
+        
         [SerializeField] private TextMeshProUGUI _textMeshPro;
-
+        
+        
         public void SetText(string text)
         {
             _textMeshPro.SetText(text);
+        }
+        
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            Clicked?.Invoke();
         }
     }
 }
