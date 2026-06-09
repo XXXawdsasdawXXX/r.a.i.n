b3eee01126a48f3479eef7567a09e2db05e4637c
=) <69734315+XXXawdsasdawXXX@users.noreply.github.com>
2026-06-09 21:21:37 +0300

    upd cards view

--- Assets/Code/UI/Windows/Game/Card/CardView.cs
+++ Assets/Code/UI/Windows/Game/Card/CardView.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using CoreGame.Card.Data;
+using Sirenix.OdinInspector;
+using Sirenix.Serialization;
 using UI.Components;
 using UnityEngine;
 using UnityEngine.UI;
@@ -21,6 +23,7 @@ namespace UI.Windows.Game.Card
             public int EnergyPrice;
             public int CurrentCharge;
             public int MaxCharge;
+            public Sprite Icon;
         }
         
         [field: SerializeField] public Model CurrentModel { get; private set; }
@@ -34,6 +37,7 @@ namespace UI.Windows.Game.Card
         [SerializeField] private UIText _textDescription;
         [SerializeField] private UIText _textCharge;
         [SerializeField] private Image _image;
+
 
         
         public override void SetInteractable(bool isInteractable)
@@ -50,6 +54,27 @@ namespace UI.Windows.Game.Card
             _textEnergyPrice.SetText(model.EnergyPrice.ToString());
             _textDescription.SetText(model.Description);
             _textCharge.SetText($"{model.CurrentCharge}/{model.MaxCharge}");
+            _image.sprite = model.Icon;
+        }
+
+        public void UpdateViewFromConfig(CardConfiguration config)
+        {
+            if (config == null)
+            {
+                return;
+            }
+
+            SetModel(new Model
+            {
+                Id = config.Id,
+                InstanceId = config.Id,
+                Name = config.Name,
+                Type = config.Type,
+                Description = config.Description,
+                EnergyPrice = config.BaseEnergyCost,
+                CurrentCharge = config.Charges,
+                MaxCharge = config.Charges
+            });
         }
 
         protected override void onClick()
@@ -66,5 +91,13 @@ namespace UI.Windows.Game.Card
 
             CardClicked = null;
         }
+
+#if UNITY_EDITOR
+        [Button("Update View From Preview Config")]
+        private void _updateViewFromPreviewConfig(CardConfiguration cardConfiguration)
+        {
+            UpdateViewFromConfig(cardConfiguration);
+        }   
+#endif
     }
 }
