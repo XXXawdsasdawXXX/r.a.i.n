a669168d869448c35203163f43322a3f87590835
=) <69734315+XXXawdsasdawXXX@users.noreply.github.com>
2026-06-10 01:31:53 +0300

    Revert "fix connection"
    This reverts commit f18dbcaa0c2610c84267217b28c05871559dbe47.

--- Assets/Code/UI/Windows/MainMenu/Connection/ConnectionWindowController.cs
+++ Assets/Code/UI/Windows/MainMenu/Connection/ConnectionWindowController.cs
@@ -10,24 +10,22 @@ namespace UI.Windows.MainMenu.Connection
     public class ConnectionWindowController : UIWindowController<ConnectionWindowView>
     {
         public bool IsInitialized { get; set; }
-
-        private ConnectionHandler _connectionHandler;
+        
+        [SerializeField] private ConnectionHandler _connectionHandler;
+        
         private GameStateMachine _gameStateMachine;
-        private bool _isConnecting;
 
         public override UniTask InitializeWindow(UIWindowManager manager)
         {
-            _connectionHandler = Container.Instance.GetService<ConnectionHandler>();
             _gameStateMachine = Container.Instance.GetService<GameStateMachine>();
-
-            string localIp = ConnectionHandler.GetLocalIPAddress();
-            string allIps = ConnectionHandler.GetAllLocalIPAddresses();
-            string ipLabel = localIp == allIps ? localIp : $"{localIp} ({allIps})";
-            view.TextUserIP.SetText($"your ip: {ipLabel}");
-
+            
+            view.TextUserIP.SetText("your ip: " + ConnectionHandler.GetLocalIPAddress());
+          
+            view.InputFieldHostIP.SetTextWithoutNotify(_connectionHandler.LastJoinedIP);
+            
             view.InputFieldHostIP.SetTextWithoutNotify(
-                PlayerPrefs.GetString(ConnectionHandler.SAVE_KEY, _connectionHandler.LastJoinedIP));
-
+                PlayerPrefs.GetString(ConnectionHandler.SAVE_KEY, view.InputFieldHostIP.Value));
+            
             return base.InitializeWindow(manager);
         }
         
@@ -58,32 +56,12 @@ namespace UI.Windows.MainMenu.Connection
 
         private void ButtonClientOnClicked()
         {
-            _connectAsClientAsync().Forget();
-        }
-
-        private async UniTaskVoid _connectAsClientAsync()
-        {
-            if (_isConnecting)
-            {
-                return;
-            }
-
-            string hostIp = view.InputFieldHostIP.Value;
-            PlayerPrefs.SetString(ConnectionHandler.SAVE_KEY, hostIp);
-
-            _isConnecting = true;
-            view.ButtonClient.SetInteractable(false);
-
-            bool connected = await _connectionHandler.ConnectAsClientAsync(hostIp);
-            _isConnecting = false;
-            view.ButtonClient.SetInteractable(true);
-
-            if (!connected)
-            {
-                return;
-            }
-
+            PlayerPrefs.SetString(ConnectionHandler.SAVE_KEY, view.InputFieldHostIP.Value);
+            
+            _connectionHandler.ConnectAsClient(view.InputFieldHostIP.Value);
+            
             _gameStateMachine.SwitchState(typeof(CoreGameState));
+            
             view.Close();
         }
 
