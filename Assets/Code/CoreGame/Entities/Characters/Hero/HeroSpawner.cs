4fbbad9ba9474d0c2676dbca41ebbd91be3961ca
=) <69734315+XXXawdsasdawXXX@users.noreply.github.com>
2026-06-12 15:47:35 +0300

    fix hero context

--- Assets/Code/CoreGame/Entities/Characters/Hero/HeroSpawner.cs
+++ Assets/Code/CoreGame/Entities/Characters/Hero/HeroSpawner.cs
@@ -53,6 +53,45 @@ namespace CoreGame.Entities.Characters.Hero
 
             base.OnStopServer();
         }
+        
+        public bool TryGetConnectionForHeroObjectId(int heroObjectId, out NetworkConnection connection)
+        {
+            foreach (KeyValuePair<NetworkConnection, NetworkObject> entry in _heroes)
+            {
+                if (entry.Value != null && entry.Value.ObjectId == heroObjectId)
+                {
+                    connection = entry.Key;
+                    return true;
+                }
+            }
+
+            connection = null;
+            return false;
+        }
+
+        public bool TryGetHeroObjectId(NetworkConnection connection, out int heroObjectId)
+        {
+            if (_heroes.TryGetValue(connection, out NetworkObject heroObject) && heroObject != null)
+            {
+                heroObjectId = heroObject.ObjectId;
+                return true;
+            }
+
+            heroObjectId = 0;
+            return false;
+        }
+
+        public bool TryGetHero(NetworkConnection connection, out Hero hero)
+        {
+            hero = null;
+            if (!_heroes.TryGetValue(connection, out NetworkObject heroObject) || heroObject == null)
+            {
+                return false;
+            }
+
+            hero = heroObject.GetComponent<Hero>();
+            return hero != null;
+        }
 
         protected override UniTask onSpawned(NetworkObject instance, NetworkConnection connection)
         {
@@ -88,7 +127,27 @@ namespace CoreGame.Entities.Characters.Hero
 
             return listeners.ToArray();
         }
-        
+
+        [ObserversRpc]
+        private void _initializeHeroComponents(NetworkObject instance)
+        {
+            Hero hero = instance.GetComponent<Hero>();
+
+            hero.InitializeComponents();
+
+            registerGameListener(getGameListeners(instance));
+            
+            HeroSpawned?.Invoke(hero);
+        }
+
+        [TargetRpc]
+        private void _setUserHero(NetworkConnection connection, NetworkObject instance)
+        {
+            UserProvider userProvider = Container.Instance.GetService<UserProvider>();
+            userProvider.SetConnection(connection);
+            userProvider.SetHero(instance);
+        }
+
         private void _sceneManagerOnClientLoadedStartScenes(NetworkConnection connection, bool isServer)
         {
             if (!isServer)
@@ -153,26 +212,6 @@ namespace CoreGame.Entities.Characters.Hero
             base.onDespawned(instance, connection);
         }
 
-        [ObserversRpc]
-        private void _initializeHeroComponents(NetworkObject instance)
-        {
-            Hero hero = instance.GetComponent<Hero>();
-
-            hero.InitializeComponents();
-
-            registerGameListener(getGameListeners(instance));
-            
-            HeroSpawned?.Invoke(hero);
-        }
-
-        [TargetRpc]
-        private void _setUserHero(NetworkConnection connection, NetworkObject instance)
-        {
-            UserProvider userProvider = Container.Instance.GetService<UserProvider>();
-            userProvider.SetConnection(connection);
-            userProvider.SetHero(instance);
-        }
-
         private void _serverManagerOnRemoveConnection(NetworkConnection connection, RemoteConnectionStateArgs state)
         {
             if (state.ConnectionState != RemoteConnectionState.Stopped)
@@ -181,45 +220,6 @@ namespace CoreGame.Entities.Characters.Hero
             }
 
             _heroes.Remove(connection);
-        }
-
-        public bool TryGetConnectionForHeroObjectId(int heroObjectId, out NetworkConnection connection)
-        {
-            foreach (KeyValuePair<NetworkConnection, NetworkObject> entry in _heroes)
-            {
-                if (entry.Value != null && entry.Value.ObjectId == heroObjectId)
-                {
-                    connection = entry.Key;
-                    return true;
-                }
-            }
-
-            connection = null;
-            return false;
-        }
-
-        public bool TryGetHeroObjectId(NetworkConnection connection, out int heroObjectId)
-        {
-            if (_heroes.TryGetValue(connection, out NetworkObject heroObject) && heroObject != null)
-            {
-                heroObjectId = heroObject.ObjectId;
-                return true;
-            }
-
-            heroObjectId = 0;
-            return false;
-        }
-
-        public bool TryGetHero(NetworkConnection connection, out Hero hero)
-        {
-            hero = null;
-            if (!_heroes.TryGetValue(connection, out NetworkObject heroObject) || heroObject == null)
-            {
-                return false;
-            }
-
-            hero = heroObject.GetComponent<Hero>();
-            return hero != null;
         }
     }
 }
